e7fb4f538907f0b9b3fe806470c00584a52faf60
Thiago Santos <thiagocroza472@gmail.com>
2022-11-02 14:16:31 -0300

    feat: add actuator

--- authuser/src/main/java/com/ead/authuser/AuthUserApplication.java
+++ authuser/src/main/java/com/ead/authuser/AuthUserApplication.java
@@ -2,12 +2,10 @@ package com.ead.authuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableFeignClients(
-        basePackages = "com.ead.clients"
-)
+@EnableEurekaClient
 public class AuthUserApplication {
 
     public static void main(String[] args) {
